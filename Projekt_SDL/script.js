d17ad305afb5945580a2d1122c86d8d88c7ee804
Jocke <jockeljung1@gmail.com>
2019-09-24 13:49:43 +0200

    Cool code

--- Projekt_SDL/script.js
+++ Projekt_SDL/script.js
@@ -1,11 +1,17 @@
 function init(){
     console.log("hello");
-    document.getElementById("button").addEventListener("click", clicked)
+    document.getElementById("button").addEventListener("click", clicked1)
     document.getElementById("button").style.display="block";
+    document.getElementById("button2").addEventListener("click", clicked2)
+    document.getElementById("button2").style.display="none";
 }
 
-function clicked(){
-    console.log("clicked");
+function clicked1(){
+    console.log("clicked 1");
+}
+
+function clicked2(){
+    console.log("clicked 2");
 }
 
 window.addEventListener("load", init);
